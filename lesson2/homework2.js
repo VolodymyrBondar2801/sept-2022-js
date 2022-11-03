6941529ce451871c678349bc929229661b8b61c8
VolodymyrBondar2801 <mrvovchikb@gmail.com>
2022-11-03 12:16:04 +0200

    lesson2_hw2

---
+++ lesson2/homework2.js
@@ -0,0 +1,37 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let arr = ['footbal', 123, 'tenis', 'golf', 'basketball', '[qwerty]', true, false, 'cat', 'dog']
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(arr[5])
+console.log(arr[6])
+console.log(arr[7])
+console.log(arr[8])
+console.log(arr[9])
+console.log(arr.length)
+
+let innerArray = arr[5]
+console.log(innerArray)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+// let book_one = {
+//     title: 'django';
+//     pageCount: 260;
+//     genre: 'the comic book';
+// }
+// console.log(book_one)
+//
+// let book_two
+// let book_three
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
