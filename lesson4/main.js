b5fc8b983c1a0699d1eb2d32a16181e2966dca6d
VolodymyrBondar2801 <mrvovchikb@gmail.com>
2022-12-01 17:03:46 +0200

    lesson5_hw5

--- lesson4/main.js
+++ lesson4/main.js
@@ -118,37 +118,37 @@
 // Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
 // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
 
-// let products = [
-//     {
-//         title: 'Milk',
-//         price: 22,
-//     image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-//     },
-//     {
-//         title: 'Juice',
-//         price: 27,
-//  image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-//     },
-//     {
-//         title: 'Tomato',
-//         price: 47,
-//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-//     },
-//     {
-//         title: 'Tea',
-//         price: 15,
-//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-//     },
-// ];
-//
-// for (const product of products)
-//
-// document.write(`<div class="product-card">
-//
-//     <h2 class="product-title"> ${product.title} :${product.price} -dollars </h2>
-// <img src="${product.image}" alt="">
-//
-// </div>`)
+let products = [
+    {
+        title: 'Milk',
+        price: 22,
+    image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'Juice',
+        price: 27,
+ image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'Tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'Tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+for (const product of products)
+
+document.write(`<div class="cards">
+
+    <h2> ${product.title} :${product.price} -dollars </h2>
+<img src="${product.image}" alt="product.title">
+
+</div>`)
 
 //
 //
